ae029c7b2f811c5d09083044db00214112809add
Iremgunes <aviremdanacioglu@gmail.com>
2022-04-28 20:56:28 +0300

    proper commit

--- src/us100/TestCase001.java
+++ src/us100/TestCase001.java
@@ -4,7 +4,7 @@ public class TestCase001 {
     public static void main(String[] args) {
         System.out.println("irem");
         System.out.println("ahmet");
-        System.out.println("bebiş");
+
     }
 
 }
